471f52f393404d66f0a5b02c35d5946a1255d0cd
jpatel531 <jamie@notespublication.com>
2014-08-24 18:30:13 +0100

    Removes origin

--- lib/come_fly_with_me.rb
+++ lib/come_fly_with_me.rb
@@ -8,6 +8,7 @@ module ComeFlyWithMe
   		system("mv sinatra-scaffold #{ARGV.first}")
   		system("sed -i.bak \"s/Waddup/#{ARGV.first}/g\" #{ARGV.first}/views/layouts/application.haml")
 		system("rm -f #{ARGV.first}/views/layouts/application.haml.bak")
+		system("git remote rm origin")
   	end
 
   end
